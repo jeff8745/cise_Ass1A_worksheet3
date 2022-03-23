8fedf179fb9bf1f49ce6c416547dab5988e2fb18
Jefferson Tsao <jeffersontsao7@gmail.com>
2022-03-23 12:56:51 +0530

    called connect to db

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('Hello world!'));
 
 const port = process.env.PORT || 8082;
