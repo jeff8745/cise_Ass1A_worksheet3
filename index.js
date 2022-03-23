e09f15c994d27769e64b3c0f75a39a8c7dda46df
Jefferson Tsao <jeffersontsao7@gmail.com>
2022-03-23 17:44:49 +0530

    added body parser

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const bookRouter = require('./routes/api/books');
-
 const app = express();
+const bodyParser = require('body-parser');
 
 // Connect Database
 connectDB();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
