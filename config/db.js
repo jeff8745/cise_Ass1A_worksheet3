4bdea4adb537b9b8c8d8cadbb288637d73090243
Jefferson Tsao <jeffersontsao7@gmail.com>
2022-03-25 18:04:22 +0530

    confgiured db.js to connection string from .env

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      db,
+      process.env.MONGODB_URI,
       {
         useNewUrlParser: true
       }
