705a9871b81900a3f5b7f099eefc634120a07605
Jefferson Tsao <jeffersontsao7@gmail.com>
2022-03-25 18:51:53 +0530

    return index if prod env

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bookRouter = require('./routes/api/books');
 const app = express();
 const bodyParser = require('body-parser');
 var cors = require('cors');
+const path = require("path");
 
 // Connect Database
 connectDB();
@@ -18,10 +19,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-app.get('/', (req, res) => res.send('Hello world!'));
-
 const port = process.env.PORT || 8082;
 
 app.use(bookRouter);
+
+if(process.env.NODE_ENV === "production")
+{
+    app.use(express.static(path.resolve(__dirname, "./book_app_client/build")));
+
+    app.get("*", function (request, response) {
+        response.sendFile(path.resolve(__dirname, "./book_app_client/build", "index.html"));
+      });
+}
+else
+{
+    app.get('/', (req, res) => res.send('Hello world!'));
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
